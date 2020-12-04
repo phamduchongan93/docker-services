fd00d09e1e78b474eed577a5a9ebae42e197c67c
an pham <vincentpham1993@gmail.com>
2020-12-04 10:18:55 -0500

    update readme.md

--- jenkins/Readme.md
+++ jenkins/Readme.md
@@ -4,7 +4,13 @@
 - 1GB+ of RAM
 - 50GB of drive space
 
-
 ## Installation
 1. To begin run `main.sh -h ` to quick view the instruction
-2. Type `main.sh build` to build the jenkins server on docker  
+2. Type `main.sh run` to build the jenkins server on docker  
+
+## Setting up Vagrant Hypervisor
+
+- In Vangrant file, I exposed the port on 192.168.18.2 ip address. Hence, please use this address to test out your services. 
+For example:
+If I have the web server on port 80, the socket I will access via web browser is 192.168.18.2:80
+
--- jenkins/main.sh
+++ jenkins/main.sh
@@ -4,24 +4,24 @@
 source modules/general.sh
 source modules/jenkins-blueoccean.sh
 
-while [ -n "$1" ]
+while [ "$#" -gt 0 ]
 do
-		case "$1" in 
-				run)
-						intro
-						run_main
-						;;
-				destroy)
-						destroyAll
-						;;
-				test) 
-						bats jenkins-blueoccean.bats;
-						;;
-				* | -*)
-						echo "Error: Invalid option"
-						help
-						exit 1
-						;;
-		esac 
-		shift
+  case "$1" in 
+    run)
+      intro
+      run_main
+      ;;
+    destroy)
+      destroyAll
+      ;;
+    test) 
+      bats jenkins-blueoccean.bats;
+      ;;
+    *)
+      echo "Error: Invalid option"
+      help
+      exit 1
+      ;;
+  esac 
+  shift
 done
