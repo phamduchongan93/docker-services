bc86f096810fa9e575c0ceade2aa684a7bb0df18
an pham <vincentpham1993@gmail.com>
2020-12-19 20:40:43 -0500

    update;

--- git_server/install.sh
+++ git_server/install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Postinstall for git server 
+preInstall () {  
+		yum install git openssh -y
+		useradd -m git 
+		groupadd git
+		su - git
+		useradd -r -m -U -d /home/git -s /bin/bash git
+  mkdir -p /home/git/.ssh/ && chmod 0700 /home/git/.ssh
+}
 
-yum install git openssh -y
-useradd -m git 
-mkdir -p /home/git/.ssh/
+preInstall 
+repoName="${1// /_}" # input parsing
 cat /vagrant_data/id_rsa.pub >> /home/git/.ssh/authorized_keys
+printf "Creating the $repoName repository ... " 
+git init --bare /home/git/$repoName.git
 
 
