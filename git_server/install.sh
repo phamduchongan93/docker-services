12b4de51cc04f14f570f9c57a6c276d608679eda
an pham <vincentpham1993@gmail.com>
2020-12-23 19:36:16 -0800

    update install.sh

--- git_server/install.sh
+++ git_server/install.sh
@@ -6,13 +6,13 @@ preInstall () {
 		groupadd git
 		su - git
 		useradd -r -m -U -d /home/git -s /bin/bash git
-  mkdir -p /home/git/.ssh/ && chmod 0700 /home/git/.ssh
+  mkdir -p /home/git/.ssh/ && chown git:git /home/git/.ssh && chmod 0700 /home/git/.ssh
 }
 
 preInstall 
 repoName="${1// /_}" # input parsing
 cat /vagrant_data/id_rsa.pub >> /home/git/.ssh/authorized_keys
 printf "Creating the $repoName repository ... " 
-git init --bare /home/git/$repoName.git
+git init --bare "/home/git/$repoName.git"
 
 
