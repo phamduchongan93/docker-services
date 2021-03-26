a4bb559f04d10040d5826f0847c9c16ac0fd21fe
Khang Tran <khangtran082005@gmail.com>
2021-03-25 20:03:34 -0700

    updated

---
+++ jenkins/jenkins.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+# Running Jenkins deployment
+
+source modules/general.sh
+source modules/jenkins-blueoccean.sh
+
+while [ "$#" -gt 0 ]
+do
+  case "$1" in 
+    run)
+      intro
+      run_main
+      ;;
+    destroy)
+      destroyAll
+      ;;
+    test) 
+      bats jenkins-blueoccean.bats;
+      ;;
+    *)
+      echo "Error: Invalid option"
+      help
+      exit 1
+      ;;
+  esac 
+  shift
+done
